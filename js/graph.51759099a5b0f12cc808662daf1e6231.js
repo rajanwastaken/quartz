ee9c36576441552d7025c6052e3cc74184db3ff2
rajanwastaken <rajanwastaken@users.noreply.github.com>
2022-11-18 02:38:59 +0000

    deploy: 9744465f74ac4bd94b6bd1d17639065a03a3b0b2

--- js/graph.51759099a5b0f12cc808662daf1e6231.js
+++ js/graph.51759099a5b0f12cc808662daf1e6231.js
@@ -155,7 +155,7 @@ async function drawGraph(baseUrl, isHome, pathColors, graphConfig) {
   const nodeRadius = (d) => {
     const numOut = index.links[d.id]?.length || 0
     const numIn = index.backlinks[d.id]?.length || 0
-    return 3 + Math.random() * (5 + numOut + numIn)
+    return 1 + Math.random() * (3 + numOut + numIn)
   }
 
   // draw individual nodes
